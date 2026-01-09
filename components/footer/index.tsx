46189fb5db4fb39c37a24d1b5c3bb1fdaad02013
jdcormack <jerethdylan@gmail.com>
2026-01-09 11:41:41 +0000

    x

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="py-4 px-6">
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 max-w-5xl w-full mx-auto py-1 px-6">
         <div className="flex flex-wrap items-center justify-center gap-4 sm:gap-6 text-sm text-black">
           <Link href="https://widgy.co" className="transition-colors">
             Home
--- components/main-navigation/index.tsx
+++ components/main-navigation/index.tsx
@@ -4,19 +4,21 @@ import { buttonVariants } from "../ui/button";
 
 export function MainNavigation() {
   return (
-    <header className="sticky top-0 flex items-center justify-between py-4 px-6 z-10 max-w-5xl w-full mx-auto bg-white">
-      <Logo />
+    <header className="sticky top-0 z-10 bg-white">
+      <div className="max-w-5xl w-full mx-auto flex items-center justify-between py-4 px-6">
+        <Logo />
 
-      <div className="flex items-center gap-2">
-        <Link
-          href="/sign-in"
-          className={buttonVariants({ variant: "outline" })}
-        >
-          Sign in
-        </Link>
-        <Link href="/sign-up" className={buttonVariants()}>
-          Get Started
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link
+            href="/sign-in"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            Sign in
+          </Link>
+          <Link href="/sign-up" className={buttonVariants()}>
+            Get Started
+          </Link>
+        </div>
       </div>
     </header>
   );

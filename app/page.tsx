31bb826e78ac63f969ab215600882225695e497b
jdcormack <jerethdylan@gmail.com>
2026-01-09 13:07:02 +0000

    x

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ export default async function HomePage() {
     <main className="relative flex flex-col min-h-screen bg-white">
       <MainNavigation />
 
-      <section className="relative flex items-center justify-center h-96 overflow-hidden max-w-6xl max-sm:w-[95%] w-full mx-auto rounded-xl">
-        <div className="absolute inset-0 bg-linear-to-br from-blue-200 via-purple-200 to-transparent blur-3xl opacity-60 z-0" />
-        <div className="relative z-10 w-full max-w-xl space-y-8 max-sm:px-5">
-          <h1 className="text-4xl font-black text-gray-900 mb-4">
-            Gather feedback. <span className="text-blue-600">Plan better.</span>{" "}
-            <span className="text-pink-600">Ship faster.</span>
-          </h1>
+      <section className="mx-5">
+        <div className="relative flex items-center justify-center h-96 overflow-hidden max-w-8xl w-full mx-auto rounded-xl m-5">
+          <div className="absolute inset-0 bg-linear-to-br from-blue-200 via-purple-200 to-transparent blur-3xl opacity-60 z-0" />
+
+          <div className="relative z-10 w-full max-w-xl space-y-8 max-sm:px-5">
+            <h1 className="text-4xl font-black text-gray-900 mb-4">
+              Gather feedback.{" "}
+              <span className="text-blue-600">Plan better.</span>{" "}
+              <span className="text-pink-600">Ship faster.</span>
+            </h1>
+          </div>
         </div>
       </section>
 
--- components/main-navigation/index.tsx
+++ components/main-navigation/index.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from "../ui/button";
 export function MainNavigation() {
   return (
     <header className="sticky top-0 z-10 bg-white">
-      <div className="max-w-5xl w-full mx-auto flex items-center justify-between py-4 px-6">
+      <div className="max-w-8xl w-full mx-auto flex items-center justify-between py-4 px-6">
         <Logo />
 
         <div className="flex items-center gap-2">
